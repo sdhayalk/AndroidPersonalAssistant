4c77049a6cfc2a757e097632f95ceb9a32f44cd3
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-03-10 19:38:53 -0700

    LoginHandler v1

--- app/src/main/java/com/example/sahil/androidpersonalassistant/LoginActivity.java
+++ app/src/main/java/com/example/sahil/androidpersonalassistant/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.sahil.androidpersonalassistant;
 
 import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,35 +12,53 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    String username, password;
     EditText usernameEditText, passwordEditText;
     Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
         NotificationManager notificationmanager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationmanager.cancel(0);
 
-        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
-        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
-        continueButton = (Button) findViewById(R.id.continueButton);
+        sharedPreferences = getSharedPreferences("PreferencesToCheckIfLoggedIn", Context.MODE_PRIVATE);
+        username = sharedPreferences.getString("username", null);
+        if(username != null)    {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("username", username);
+            bundle.putString("password", password);
+            intent.putExtras(bundle);
+            startActivity(intent);
+        }
+        else {
+            setContentView(R.layout.activity_login);
 
-        continueButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if(usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")) {
-                    Toast.makeText(LoginActivity.this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
+            usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+            passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+            continueButton = (Button) findViewById(R.id.continueButton);
+
+            continueButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")) {
+                        Toast.makeText(LoginActivity.this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
+                    } else {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username", usernameEditText.getText().toString());
+                        editor.putString("password", passwordEditText.getText().toString());
+                        editor.commit();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("username", usernameEditText.getText().toString());
+                        bundle.putString("password", passwordEditText.getText().toString());
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
                 }
-                else {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    Bundle bundle = new Bundle();
-                    bundle.putString("username", usernameEditText.getText().toString());
-                    bundle.putString("password", passwordEditText.getText().toString());
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-                }
-            }
-        });
+            });
+        }
     }
 }
