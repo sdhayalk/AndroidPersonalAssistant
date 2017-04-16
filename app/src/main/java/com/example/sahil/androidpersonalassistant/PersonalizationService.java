e7b19bb2012ac277ed870d58080300e22918337f
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2017-04-16 13:24:21 -0700

    added ml dependencies

--- app/src/main/java/com/example/sahil/androidpersonalassistant/PersonalizationService.java
+++ app/src/main/java/com/example/sahil/androidpersonalassistant/PersonalizationService.java
@@ -49,7 +49,6 @@ public class PersonalizationService extends Service {
     @Override
     public void onStart(Intent intent, int startId) {
         Toast.makeText(this, " MyService Started", Toast.LENGTH_SHORT).show();
-        Log.d("Started", "started");
 
         checkForFileAndFolder();
 
